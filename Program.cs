758e7f8dcf8ea5f6a4105b4dae9ccf2f58393895
Savenok <Romasav2017@gmail.com>
2022-09-21 07:37:45 +0300

    47

--- Program.cs
+++ Program.cs
@@ -1,28 +1,25 @@
-﻿// Задача 51: Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали (с индексами (0,0); (1;1) и т.д.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// Сумма элементов главной диагонали: 1+9+2 = 12
-
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4     // 0,5 7 -2 -0,2    // 1 -3,3 8 -9,9    // 8 7,8 -7,1 9
 Console.Clear();
 Console.Write("Введите колличество строк массива: ");
 int rows = int.Parse(Console.ReadLine());
+
 Console.Write("Введите колличество колоннок массива: ");
-int colomns = int.Parse(Console.ReadLine());
-int[,] GetArray(int m, int n, int minValue, int maxValue)
+int  colomns = int.Parse(Console.ReadLine());
+
+double[,] GetArray(int m, int n, int minValue, int maxValue)
 {
-    int[,] result = new int[m, n];
+    double[,] result = new double[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            result[i, j] = new Random().Next(minValue, maxValue);
+            result[i, j] = Convert.ToDouble(new Random().Next(minValue, maxValue) / 10.0);
         }
     }
     return result;
 }
-void PrintArray(int[,] inarray)
+void PrintArray(double[,] inarray)
 {
     for (int i = 0; i < inarray.GetLength(0); i++)
     {
@@ -33,24 +30,5 @@ void PrintArray(int[,] inarray)
         Console.WriteLine();
     }
 }
-int SumElementsOnDiagonals(int[,] inarray1)
-{
-     int sum = 0;
-    for (int i = 0; i < inarray1.GetLength(0); i++)
-    {
-        for (int j = 0; j < inarray1.GetLength(1); j++)
-        {
-            if ( i == j) sum =  sum + inarray1[i,j] ;
-        }
-    }
-    return sum;
-}
-void PrintSum( int sumdiagonals)
-{
-   Console.WriteLine(sumdiagonals);
-}
-int[,] array = GetArray(rows, colomns, minValue: 1, maxValue: 10); // Не МАГИЧЕСКОЕ ЧИСЛО))
+double[,] array = GetArray(rows, colomns, minValue: 0, maxValue: 100);
 PrintArray(array);
-int summ = Convert.ToInt32(SumElementsOnDiagonals(array));
-Console.WriteLine();
-PrintSum(summ);
